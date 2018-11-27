0b342d5652a58942c667a2e4d67486095ea8082a
juarisco <juarisco@outlook.com>
2018-11-27 10:54:07 -0500

    Configurando laravel para el projecto clientehttp

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace ClienteHTTP\Http\Middleware;
 
 use Illuminate\Http\Request;
 use Fideloper\Proxy\TrustProxies as Middleware;
--- config/app.php
+++ config/app.php
@@ -171,11 +171,11 @@ return [
         /*
          * Application Service Providers...
          */
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
-        // App\Providers\BroadcastServiceProvider::class,
-        App\Providers\EventServiceProvider::class,
-        App\Providers\RouteServiceProvider::class,
+        ClienteHTTP\Providers\AppServiceProvider::class,
+        ClienteHTTP\Providers\AuthServiceProvider::class,
+        // ClienteHTTP\Providers\BroadcastServiceProvider::class,
+        ClienteHTTP\Providers\EventServiceProvider::class,
+        ClienteHTTP\Providers\RouteServiceProvider::class,
 
     ],
 
